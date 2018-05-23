8f591aaa02378830234192ef6589a7feaac92117
Jiri Hajek <jiri.hajek18@gmail.com>
2018-05-23 23:09:00 +0200

    + finalize notes, order by priority (top=highest)

--- src/main.cpp
+++ src/main.cpp
@@ -5,20 +5,22 @@
 /*
  3 - CHECK
  * TODO:
- * - document new files for doxygen
- * - ghost movement
- 2 - ghost AI - chase player; chase player if closer than X; find and destroy power-ups, otherwise random-ish
- 2 - implement bfs for ghosts, update shortest path to player on every turn
+ * - end state - screen with score & turns, if pacman is alive => victory, loss otherwise
  1 - pacman power-up => for X turns, pacman has the ability to eat 1 ghost
- * - if ghost makes a move after pacman, they can only end up stacked in this order: GameObject=>Pacman=>Ghost
- * - fix for MovingGameObject carry is already implemented, not tested though
+ * - split 'Play' to 2 game modes in menu
  ! - gamemode ideas:
  4   1) survive for X turns, where X is loaded from config
  4   2) collect all coins in X turns ( 0 for no limit ), where X is loaded from config
- * - bring 'reload cfg' option back
- * - split 'Play' to 2 game modes in menu
- 5 - ghost difficulty => DFS x BFS (?), sounds hard though
+ * - ghost movement
+ 2 - ghost AI - chase player; chase player if closer than X; find and destroy power-ups, otherwise random-ish
+ 2 - implement bfs for ghosts, update shortest path to player on every turn
  5 - ghost difficulty => every Xth turn of ghost is random, where X is loaded from config
+ 5 - ghost difficulty => DFS x BFS (?), sounds hard though
+ * - document new files for doxygen
+ * - bring 'reload cfg' option back
+ -------------------------------------------------------------------------------
+ * - if ghost makes a move after pacman, they can only end up stacked in this order: GameObject=>Pacman=>Ghost
+ * - fix for MovingGameObject carry is already implemented, not tested though
  */
 #include <iostream>
 #include <ncurses.h>
